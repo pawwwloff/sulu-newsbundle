b78b85f99b387b22fbf02aa7118ec4d029850065
Matthieu <support@pixel-developpement.com>
2022-04-04 17:45:09 +0200

    + Ajout configuration

---
+++ Entity/Setting.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Pixel\NewsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use Sulu\Bundle\MediaBundle\Entity\MediaInterface;
+use Sulu\Component\Persistence\Model\AuditableInterface;
+use Sulu\Component\Persistence\Model\AuditableTrait;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="news_settings")
+ * @Serializer\ExclusionPolicy("all")
+ */
+class Setting implements AuditableInterface
+{
+    use AuditableTrait;
+
+    public const RESOURCE_KEY = "news_settings";
+    public const FORM_KEY = "news_settings";
+    public const SECURITY_CONTEXT = "news_settings.settings";
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Serializer\Expose()
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=MediaInterface::class)
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     * @Serializer\Expose()
+     */
+    private ?MediaInterface $defaultImage = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getDefaultImage(): ?MediaInterface
+    {
+        return $this->defaultImage;
+    }
+
+    /**
+     * @param MediaInterface|null $defaultImage
+     */
+    public function setDefaultImage(?MediaInterface $defaultImage): void
+    {
+        $this->defaultImage = $defaultImage;
+    }
+}
---
+++ Twig/SettingsExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pixel\NewsBundle\Twig;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Pixel\GalleryBundle\Entity\Setting;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class SettingsExtension extends AbstractExtension
+{
+    private $entityManager;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('news_settings', [$this, 'newsSettings']),
+        ];
+    }
+
+    public function newsSettings()
+    {
+        return $this->entityManager->getRepository(Setting::class)->findOneBy([]);
+    }
+}
